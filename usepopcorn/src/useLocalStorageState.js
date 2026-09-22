3cf3ec641bbfbac0626748008372183b5a406229
Aditya Singh <98811381+adityapratap107@users.noreply.github.com>
2026-09-22 23:03:59 +0530

    Implemented custom hooks

---
+++ usepopcorn/src/useLocalStorageState.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(() => {
+    const stored = localStorage.getItem(key);
+    return stored ?  JSON.parse(stored) : initialState;
+  });
+
+    useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+
+  return [value, setValue];
+
+}
