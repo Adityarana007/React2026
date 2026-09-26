c260f9aba8f4da1042ebc1c26f8367e64048d4e0
Aditya Singh <98811381+adityapratap107@users.noreply.github.com>
2026-09-26 23:43:08 +0530

    implemented react quiz with useReducer

---
+++ react-quiz/src/components/Timer.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+function Timer({dispatch, secondsRemaining}) {
+    const mins = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            console.log('tick')
+            dispatch({type: "tick"})
+        }, 1000)
+        return () => clearInterval(timerId)
+    }, [dispatch])
+  return (
+    <div className='timer'>{mins < 10 && "0"}{mins}:{seconds < 10 && "0"}{seconds}</div>
+  )
+}
+
+export default Timer
